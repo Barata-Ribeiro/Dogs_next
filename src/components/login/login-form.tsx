b3b41aa4c6a0bca146c4a7959e21399dfb8a0b60
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-03-22 07:10:06 -0300

    feat: implement all login page styles

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -4,8 +4,10 @@ import login from '@/actions/login';
 import Button from '@/components/forms/button';
 import Input from '@/components/forms/input';
 import ErrorMessage from '@/components/helper/error-message';
+import Link from 'next/link';
 import { useEffect } from 'react';
 import { useFormState, useFormStatus } from 'react-dom';
+import styles from './login-form.module.css';
 
 function FormButton() {
   const { pending } = useFormStatus();
@@ -29,7 +31,7 @@ export default function LoginForm() {
 
   return (
     <>
-      <form action={action}>
+      <form className={styles.form} action={action}>
         <Input
           label='Usuário'
           name='username'
@@ -46,6 +48,14 @@ export default function LoginForm() {
         <ErrorMessage error={state.error} />
         <FormButton />
       </form>
+      <Link className={styles.recuperar} href='/login/perdeu'>
+        Perdeu a senha?
+      </Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link className='button' href='/login/criar'>Cadastro</Link>
+      </div>
     </>
   );
 }
