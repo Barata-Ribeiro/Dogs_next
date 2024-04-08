5ca30df6aa4cbd14b1d0b713cc6bd1c4a136176f
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-04-08 07:12:20 -0300

    JWT Verify

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,10 @@
 import { NextResponse, type NextRequest } from 'next/server';
+// import verifyToken from '@/functions/verify-token';
 
 export async function middleware(req: NextRequest) {
   const token = req.cookies.get('authToken')?.value;
   const authenticated = !!token;
+  // const authenticated = token ? await verifyToken(token) : false;
 
   if (!authenticated && req.nextUrl.pathname.startsWith('/conta')) {
     return NextResponse.redirect(new URL('/login', req.url));
